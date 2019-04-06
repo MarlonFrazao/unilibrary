4fce76d357c47924efcb3b2df4238f81eeed59c4
MarlonFrazao <36695923+MarlonFrazao@users.noreply.github.com>
2019-04-06 11:39:13 -0300

    Autor concluído, mas ainda sem teste.
    AutorRest pode conter erros, pois foi codificado sem o uso de IDE.

---
+++ PrjIntegradorV/src/br/edu/unifacear/prjintegrador5/rest/AutorRest.java
@@ -0,0 +1,93 @@
+package br.edu.unifacear.prjintegrador5.rest;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import br.edu.unifacear.prjintegrador5.entity.Autor;
+import br.edu.unifacear.prjintegrador5.business.AutorBusiness;
+
+@Path("/autor")
+public class AutorRest {  
+        private AutorBusiness business;
+        
+        public AutorRest() {
+                business = new AutorBusiness();
+        }
+        
+        @Path("/inserir")
+        @Consumes("application/json; charset=UTF-8")
+        @POST
+        public Response inserir(Autor a) {
+                try {
+                        business.inserir(a);
+                        return Response.status(200).entity("Autor cadastrado com sucesso!").build();
+                } catch(Exception e) {
+                        return Response.status(200).entity("Erro: " + e.getMessage()).build();
+                }
+        }
+        
+        @Path("/alterar")
+        @Consumes("application/json; charset=UTF-8")
+        @PUT
+        public Response alterar(Autor a) {
+                try {
+                        business.alterar(a);
+                        return Response.status(200).entity("Autor atualizado com sucesso!").build();
+                } catch(Exception e) {
+                        return Response.status(200).entity("Erro: " + e.getMessage()).build();
+                }
+        }
+        
+        @Path("/listar")
+        @Produces("application/json; charset=UTF-8")
+        @GET
+        public Response listar() {
+                try {
+                        return Response.status(200).entity(business.listar()).build();
+                } catch(Exception e) {
+                        return Response.status(200).entity("Erro: " + e.getMessage()).build();
+                }
+        }
+        
+        @Path("/obter_id")
+        @Produces("application/json; charset=UTF-8")
+        @Consumes("application/json; charset=UTF-8")
+        @POST
+        public Response obterId(Autor a) {
+                try{
+                        return Response.status(200).entity(business.obter(a.getId())).build();
+                } catch(Exception e) {
+                        return Response.status(200).entity("Erro: " + e.getMessage()).build();
+                }
+        }
+        
+        @Path("/obter_nome")
+        @Produces("application/json; charset=UTF-8")
+        @Consumes("application/json; charset=UTF-8")
+        @POST
+        public Response obterNome(Autor a) {
+                try {
+                        return Response.status(200).entity(business.obter(a.getNome())).build();
+                } catch(Exception e) {
+                        return Response.status(200).entity("Erro: " + e.getMessage()).build();
+                }
+        }
+        
+        @Path("/excluir")
+        @Consumes("application/json; charset=UTF-8")
+        @DELETE
+        public Response excluir(Autor a) {
+                try {
+                        business.excluir(a);
+                        return Response.status(200).entity("Autor excluído com sucesso!").build();
+                } catch(Exception e) {
+                        return Response.status(200).entity("Erro: " + e.getMessage()).build();
+                }
+        }
+}
