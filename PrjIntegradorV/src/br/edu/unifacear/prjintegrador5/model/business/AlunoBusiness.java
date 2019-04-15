5eca3342d7467ce484d4055a4745703905073347
MarlonFrazao <36695923+MarlonFrazao@users.noreply.github.com>
2019-04-15 13:15:04 -0300

    AlunoBusiness criado vazio

---
+++ PrjIntegradorV/src/br/edu/unifacear/prjintegrador5/model/business/AlunoBusiness.java
@@ -0,0 +1,12 @@
+package br.edu.unifacear.prjintegrador5.model.business;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.edu.unifacear.prjintegrador5.model.dao.AlunoDAO;
+import br.edu.unifacear.prjintegrador5.model.entity.Aluno;
+
+public class AlunoBusiness {
+
+
+}
