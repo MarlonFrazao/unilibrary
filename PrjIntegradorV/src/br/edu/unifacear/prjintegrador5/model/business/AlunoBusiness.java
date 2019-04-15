d1fee279a0fa9b89250dc2694dce6ceaf579bdb8
MarlonFrazao <36695923+MarlonFrazao@users.noreply.github.com>
2019-04-15 13:32:36 -0300

    Criado método inserir aluno na camada de negócio

--- PrjIntegradorV/src/br/edu/unifacear/prjintegrador5/model/business/AlunoBusiness.java
+++ PrjIntegradorV/src/br/edu/unifacear/prjintegrador5/model/business/AlunoBusiness.java
@@ -5,8 +5,37 @@ import java.util.List;
 
 import br.edu.unifacear.prjintegrador5.model.dao.AlunoDAO;
 import br.edu.unifacear.prjintegrador5.model.entity.Aluno;
+import br.edu.unifacear.prjintegrador5.model.entity.Curso;
 
 public class AlunoBusiness {
-
-
+        private AlunoDAO dao;
+        
+        public AlunoBusiness() {
+          			dao = new AlunoDAO();
+        }
+				
+				public void inserir(Aluno a) throws BusinessException {
+								List<Aluno> lista = new ArrayList<Aluno>();
+							
+								if(lista.size() < 1) {
+											dao.inserir(a);	
+								} else {
+											Boolean aux = false;
+									
+											for(Aluno al : lista) {
+											  		if(al.getMatricula() == a.getMatricula()) {
+																	aux = false;	
+																	break;
+														} else {
+																	aux = true;	
+														}
+											}
+											
+											if(aux) {
+														dao.inserir(a);	
+											} else {
+														throw new BusinessException("Aluno já cadastrado!");	
+											}
+								}
+				}
 }
