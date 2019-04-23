391c09656c984bfb51c56df7555403e7d80cf6dc
MarlonFrazao <36695923+MarlonFrazao@users.noreply.github.com>
2019-04-23 11:44:04 -0300

    UsuarioBusiness concluido. teste pendente

--- PrjIntegradorV/src/br/edu/unifacear/prjintegrador5/model/business/UsuarioBusiness.java
+++ PrjIntegradorV/src/br/edu/unifacear/prjintegrador5/model/business/UsuarioBusiness.java
@@ -1,5 +1,8 @@
 package br.edu.unifacear.prjintegrador5.model.business;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.edu.unifacear.prjintegrador5.model.dao.UsuarioDAO;
 import br.edu.unifacear.prjintegrador5.model.entity.Usuario;
 
@@ -19,8 +22,124 @@ public class UsuarioBusiness {
 			try {
 				dao.inserir(u);
 			} catch(Exception e) {
-				throw new BusinessException("Erro na conex�o com o banco de dados: " + e.getMessage());
+				throw new BusinessException("Erro na conexão com o banco de dados: " + e.getMessage());
 			}
 		}
+	}
+	
+	public void alterar(Usuario u) throws BusinessException {
+		if(dao.obterPorId(i.getId()).getId() == null) {
+			throw new BusinessException("Usuario nao encontrado"!);
+		} else {
+			try {
+				dao.alterar(u);
+			} catch(Exception e) {
+				throw new BusinessException("Erro na conexão com o banco de dados: " + e.getMessage());
+			}
+		}
+	}
+	
+	public List<Usuario> listar() throws BusinessException {
+		List<Usuario> lista = new ArrayList<Usuario>();
+		
+		try {
+			lista = dao.listar();
+		} catch(Exception e) {
+			throw new BusinessException("Erro na conexão com o banco de dados: " + e.getMessage());
+		}
+		
+		if(lista.size() < 1) {
+			throw new BusinessException("Nao ha usuarios cadastrados");
+		}
+		
+		return lista;
+	}
+	
+	public Usuario obterPorId(int id) throws BusinessException {
+		Usuario u = new Usuario();
+		
+		try {
+			u = dao.obterPorId(id);
+		} catch(Exception e) {
+			throw new BusinessException("Erro na conexão com o banco de dados: " + e.getMessage());
+		}
+		
+		if(u.getId() == null) {
+			throw new BusinessException("Usuario nao encontrado!");
+		}
+		
+		return u;
+	}
+	
+	public Usuario obterPorUsuario(int usuario) throws BusinessException {
+		Usuario u = new Usuario();
+		
+		try {
+			u = dao.obterPorUsuario(usuario);
+		} catch(Exception e) {
+			throw new BusinessException("Erro na conexão com o banco de dados: " + e.getMessage());
+		}
+		
+		if(u.getId() == null) {
+			throw new BusinessException("Usuario nao encontrado!");
+		}
+		
+		return u;
+	}
+	
+	public void alterarStatus(Usuario u) throws BusinessException {
+		Usuario us = new Usuario();
+		
+		try {
+			us = dao.obterPorId(u.getId());
+			
+			if(us.getId() != null) {
+				if(us.getStatus) {
+					u.setStatus(false);
+				} else {
+					u.setStatus(true);
+				}
+			
+				dao.alterar(u);
+			} else {
+				throw new BusinessException("Usuario nao encontrado!");
+			}
+		} catch(Exception e) {
+			throw new BusinessException("Erro na conexão com o banco de dados: " + e.getMessage());
+		}
+	}
+	
+	public void excluir(Usuario u) throws BusinessException {
+		try {
+			if(dao.obterPorId(u.getId()).getId() == null) {
+				throw new BusinessException("Usuario nao encontrado!");
+			} else {
+				dao.excluir(u);
+			}
+		} catch(Exception e) {
+			throw new BusinessException("Erro na conexão com o banco de dados: " + e.getMessage());
+		}
+	}
+	
+	public Boolean login(int usuario, int senha)  throws BusinessException {
+		Boolean aceito = false;
+		
+		try {
+			Usuario u = dao.obterPorUsuario(usuario);
+			
+			if(u.getId() == null) {
+				throw new BusinessException("Usuario nao encontrado!");
+			} else {
+				if(u.getSenha() == senha) {
+					aceito = true;
+				} else {
+					aceito = false;
+				}
+			}
+		} catch(Exception e) {
+			throw new BusinessException("Erro na conexão com o banco de dados: " + e.getMessage());
+		}
+		
+		return aceito;
 	}
 }
