79968196ce0478eb4d9e803001a44519223a2ae1
MarlonFrazao <36695923+MarlonFrazao@users.noreply.github.com>
2019-04-23 14:59:12 -0300

    UsuarioRest Criado, teste pendente.

---
+++ PrjIntegradorV/src/br/edu/unifacear/prjintegrador5/rest/UsuarioRest.java
@@ -0,0 +1,117 @@
+package br.edu.unifacear.prjintegrador5.rest;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import br.edu.unifacear.prjintegrador5.model.business.UsuarioBusiness;
+import br.edu.unifacear.prjintegrador5.model.entity.Usuario;
+
+@Path("/usuario")
+public class UsuarioRest {
+	private UsuarioBusiness business;
+	
+	public UsuarioRest() {
+		business = new UsuarioBusiness();
+	}
+	
+	@POST
+	@Consumes("application/json; charset=UTF-8")
+	@Path("/inserir")
+	public Response inserir(Usuario u) {
+		try {
+			business.inserir(u);
+			return Response.status(201).entity("Usuario cadastrado com sucesso!").build();
+		} catch(Exception e) {
+			return Response.status(201).entity("Erro: " + e.getMessage()).buld();
+		}
+	}
+	
+	@PUT
+	@Consumes("application/json; charset=UTF-8")
+	@Path("/alterar")
+	public Response alterar(Usuario u) {
+		try {
+			business.alterar(u);
+			return Response.status(201).entity("Usuario alterado com sucesso!").build();
+		} catch(Exception e) {
+			return Response.status(201).entity("Erro: " + e.getMessage()).buld();
+		}
+	}
+	
+	@GET
+	@Produces("application/json; charset=UTF-8")
+	@Path("/listar")
+	public Response listar() {
+		try {
+			return Response.status(201).entity(business.listar()).buld();
+		} catch(Exception e) {
+			return Response.status(201).entity("Erro: " + e.getMessage()).buld();
+		}
+	}
+	
+	@POST
+	@Consumes("application/json; charset=UTF-8")
+	@Produces("application/json; charset=UTF-8")
+	@Path("/obter/id")
+	public Response obterPorId(Usuario u) {
+		try {
+			return Response.status(201).entity(business.obterPorId(u.getId())).buld();
+		} catch(Exception e) {
+			return Response.status(201).entity("Erro: " + e.getMessage()).buld();
+		}
+	}
+	
+	@POST
+	@Consumes("application/json; charset=UTF-8")
+	@Produces("application/json; charset=UTF-8")
+	@Path("/obter/usuario")
+	public Response obterPorUsuario(Usuario u) {
+		try {
+			return Response.status(201).entity(business.obterPorId(u.getUsuario())).buld();
+		} catch(Exception e) {
+			return Response.status(201).entity("Erro: " + e.getMessage()).buld();
+		}
+	}
+	
+	@PUT
+	@Consumes("application/json; charset=UTF-8")
+	@Path("/alterar/status")
+	public Response alterarStatus(Usuario u) {
+		try {
+			business.alterarStatus(u);
+			return Response.status(201).entity("Status alterado com sucesso!").build();
+		} catch(Exception e) {
+			return Response.status(201).entity("Erro: " + e.getMessage()).buld();
+		}
+	}
+	
+	@DELETE
+	@Consumes("application/json; charset=UTF-8")
+	@Path("/excluir")
+	public Response excluir(Usuario u) {
+		try {
+			business.excluir(u);
+			return Response.status(201).entity("Usuario excluido com sucesso!").build();
+		}catch(Exception e) {
+			return Response.status(201).entity("Erro: " + e.getMessage()).buld();
+		}
+	}
+	
+	@POST
+	@Consumes("application/json; charset=UTF-8")
+	@Produces("application/json; charset=UTF-8")
+	@Path("/login")
+	public Response login(Usuario u) {
+		try {
+			return Response.status(201).entity(business.login(u.getUsuario(), u.getSenha()).build();
+		} catch(Exception e) {
+			return Response.status(201).entity("Erro: " + e.getMessage()).buld();
+		}
+	}
+}
